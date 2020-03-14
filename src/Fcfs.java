bbb50f3a00a350d83083221a8f6f44fb797cee6c
hclt <hans.tan@obf.ateneo.edu>
2020-03-14 17:29:15 +0800

    Round Robin not yet done

--- src/Fcfs.java
+++ src/Fcfs.java
@@ -4,83 +4,51 @@ public class Fcfs {
     private int at[];
     private int bt[];
     private int nl[];
+    private int indexes[];
 
     //a = Arrival Time , b = Burst Time , c = Nice Level , length of any of the arrays is the # of processors
     public Fcfs(int a[], int b[], int c[]) {
         at = a;
         bt = b;
         nl = c;
+        indexes = new int[at.length];
+        //Make index arrays
+        for(int i = 1; i <= at.length; i++) {
+            indexes[i-1] = i;
+        }
     }
 
     //METHODS FOR INSTRUCTIONS
     //a = Arrival Time , b = Burst Time , c = Nice Level , length of any of the arrays is the # of processors
-    public String fcfsFunc(int a[], int b[], int c[]) {
+    public String fcfsFunc() {
 
         //Output Arrangement
-        int out[] = new int[a.length];
+        int out[] = new int[at.length];
 
         //Output Waiting Times
-        int outWait[] = new int[a.length];
+        int outWait[] = new int[at.length];
 
         //Output Turnaround Times
-        int outTurn[] = new int[a.length];
+        int outTurn[] = new int[at.length];
 
         //Output Response Times
-        int outResponse[] = new int[a.length];
+        int outResponse[] = new int[at.length];
 
-        //Make index arrays
-        int indexes[] = new int[a.length];
-        for(int i = 1; i <= a.length; i++) {
-
-            indexes[i-1] = i;
-
-        }
-
-        //Sort by arrival time
-        for(int i = 0; i < a.length - 1; i ++) {
-
-            for(int j = 0; j < a.length-i-1; j++) {
-
-                if(a[j] > a[j+1]) {
-
-                   //Sorts arrival time
-                   int temp = a[j];
-                   a[j] = a[j+1];
-                   a[j+1] = temp;
-
-                   //Sorts burst time
-                   temp = b[j];
-                   b[j] = b[j+1];
-                   b[j+1] = temp;
-
-                   //Sorts nice level
-                   temp = c[j];
-                   c[j] = c[j+1];
-                   c[j+1] = temp;
-
-                   //Sorts the indexes
-                   temp = indexes[j];
-                   indexes[j] = indexes[j+1];
-                   indexes[j+1] = temp;
-
-                }
-
-            }
-
-        }
+        //Sort Arrival Time
+        sortOrderByArrival();
 
         //Output String
         String outputText = "";
 
         //Order of the processes
-        outputText += printOrder(a,b,c,indexes);
+        outputText += printOrder(at,bt,nl,indexes);
 
         //Waiting Time
         outWait[0] = 0;
 
         for (int i = 1; i < out.length; i++ ) {
 
-            outWait[i] = a[i-1] + b[i-1] + outWait[i-1];
+            outWait[i] = at[i-1] + bt[i-1] + outWait[i-1];
 
         }
 
@@ -88,7 +56,7 @@ public class Fcfs {
 
         for (int i = 0; i < outTurn.length; i++ ) {
 
-            outTurn[i] = b[i] + outWait[0];
+            outTurn[i] = bt[i] + outWait[0];
 
         }
 
@@ -98,7 +66,7 @@ public class Fcfs {
     }
 
     public String solveAll() {
-        String output = fcfsFunc(at,bt,nl);
+        String output = fcfsFunc();
         return output;
     }
 
@@ -110,6 +78,58 @@ public class Fcfs {
             output = output + "\n";
         }
         return output;
+    }
+
+    //Function to sort the order of the arrays by arrival time
+    public void sortOrderByArrival() {
+        //Sort by arrival time
+        for(int i = 0; i < at.length - 1; i ++) {
+
+            for(int j = 0; j < at.length-i-1; j++) {
+
+                if(at[j] > at[j+1]) {
+
+                    //Sorts arrival time
+                    int temp = at[j];
+                    at[j] = at[j+1];
+                    at[j+1] = temp;
+
+                    //Sorts burst time
+                    temp = bt[j];
+                    bt[j] = bt[j+1];
+                    bt[j+1] = temp;
+
+                    //Sorts nice level
+                    temp = nl[j];
+                    nl[j] = nl[j+1];
+                    nl[j+1] = temp;
+
+                    //Sorts the indexes
+                    temp = indexes[j];
+                    indexes[j] = indexes[j+1];
+                    indexes[j+1] = temp;
+
+                }
+
+            }
+
+        }
+    }
+
+    public int[] getAt() {
+        return at;
+    }
+
+    public int[] getBt() {
+        return bt;
+    }
+
+    public int[] getNl() {
+        return nl;
+    }
+
+    public int[] getIndexes() {
+        return indexes;
     }
 
 }
--- src/RoundRobin.java
+++ src/RoundRobin.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+
 public class RoundRobin {
 
     private int at[];
     private int bt[];
     private int nl[];
     private int Q;
+    private int indexes[];
+    private Fcfs fcfs;
 
     //a = Arrival Time , b = Burst Time , c = Nice Level , length of any of the arrays is the # of processors
     public RoundRobin(int a[], int b[], int c[], int q) {
@@ -11,21 +15,82 @@ public class RoundRobin {
         bt = b;
         nl = c;
         Q = q;
+        indexes = new int[at.length];
+        //Make index arrays
+        for(int i = 1; i <= at.length; i++) {
+            indexes[i-1] = i;
+        }
+        fcfs = new Fcfs(at,bt,nl);
     }
 
-    public String rrFunc(int a[], int b[], int c[], int Q) {
+    public String rrFunc() {
 
         //Output Arrangement
-        int out[] = new int[a.length];
+        int out[] = new int[at.length];
 
         //Output Waiting Times
-        int outWait[] = new int[a.length];
+        int outWait[] = new int[at.length];
 
         //Output Turnaround Times
-        int outTurn[] = new int[a.length];
+        int outTurn[] = new int[at.length];
 
         //Output Response Times
-        int outResponse[] = new int[a.length];
+        int outResponse[] = new int[at.length];
+
+        //Sort by arrival time
+        fcfs.sortOrderByArrival();
+        at = fcfs.getAt();
+        bt = fcfs.getBt();
+        nl = fcfs.getNl();
+        indexes = fcfs.getIndexes();
+        int currentSecondsCount = 0;
+        int i = 0;
+        int currentProcessIndex = i;
+        boolean hasStartedProcess = false;
+        boolean isCurrentProcessDone = false;
+
+        ArrayList<Integer> indexList = new ArrayList<>();
+
+        for(int seconds = 0; seconds<10000000; seconds++) {
+
+            if(seconds == at[i] || !hasStartedProcess || currentSecondsCount == Q || isCurrentProcessDone) {
+
+                if(currentSecondsCount == Q && !isCurrentProcessDone) {
+                    indexList.add(i);
+                    i++;
+                    currentSecondsCount = 0;
+                } else if(isCurrentProcessDone && !hasStartedProcess) {
+
+                    i++;
+                    currentSecondsCount = 0;
+
+                } else if(!isCurrentProcessDone && !hasStartedProcess) {
+
+                    hasStartedProcess = true;
+
+                }
+            }
+
+            if(hasStartedProcess && currentSecondsCount != Q) {
+
+
+
+            } else {
+
+                currentSecondsCount++;
+
+            }
+
+            //WAIT TIME COUNTING
+            if(!indexList.isEmpty()) {
+                for (int j = 0; j < indexList.size(); j++) {
+
+
+
+                }
+            }
+
+        }
 
         //Output String
         String outputText = "";
@@ -34,8 +99,8 @@ public class RoundRobin {
     }
 
     public String solveAll() {
-        String output = rrFunc(at,bt,nl,Q);
-        return null;
+        String output = rrFunc();
+        return output;
     }
 
 }
