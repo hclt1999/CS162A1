4498db92be18b9802976b07258ce6740148350a5
hclt <hans.tan@obf.ateneo.edu>
2020-03-16 20:05:27 +0800

    Round Robin and FCFS with computations

--- src/Fcfs.java
+++ src/Fcfs.java
@@ -1,20 +1,48 @@
+import java.util.ArrayList;
 
 public class Fcfs {
 
     private int at[];
     private int bt[];
     private int nl[];
-    private int indexes[];
+    private int indices[];
+    private int outWait[];
+    private int outResponse[];
+    private int outTurn[];
+    private int dupBt[];
 
     //a = Arrival Time , b = Burst Time , c = Nice Level , length of any of the arrays is the # of processors
     public Fcfs(int a[], int b[], int c[]) {
         at = a;
         bt = b;
         nl = c;
-        indexes = new int[at.length];
+        indices = new int[at.length];
         //Make index arrays
         for(int i = 1; i <= at.length; i++) {
-            indexes[i-1] = i;
+            indices[i-1] = i;
+        }
+
+        outWait = new int[at.length];
+        outTurn = new int[at.length];
+        outResponse = new int[at.length];
+
+        for (int j=0; j<outWait.length; j++) { //initialize all wait times to 0
+            outResponse[j]=-1;
+        }
+
+        outTurn = new int[at.length];
+        for (int j=0; j<outWait.length; j++) { //initialize all wait times to 0
+            outTurn[j]=-1;
+        }
+
+        outWait = new int[at.length];
+        for (int j=0; j<outWait.length; j++) { //initialize all wait times to 0
+            outWait[j]=-1;
+        }
+
+        dupBt = new int[bt.length];
+        for (int j=0; j<dupBt.length; j++) { //initialize all wait times to 0
+            dupBt[j]=bt[j];
         }
     }
 
@@ -22,61 +50,133 @@ public class Fcfs {
     //a = Arrival Time , b = Burst Time , c = Nice Level , length of any of the arrays is the # of processors
     public String fcfsFunc() {
 
-        //Output Arrangement
-        int out[] = new int[at.length];
-
-        //Output Waiting Times
-        int outWait[] = new int[at.length];
-
-        //Output Turnaround Times
-        int outTurn[] = new int[at.length];
-
-        //Output Response Times
-        int outResponse[] = new int[at.length];
-
         //Sort Arrival Time
         sortOrderByArrival();
 
         //Output String
         String outputText = "";
 
-        //Order of the processes
-        outputText += printOrder(at,bt,nl,indexes);
 
-        //Waiting Time
-        outWait[0] = 0;
+        //COUNTS
 
-        for (int i = 1; i < out.length; i++ ) {
+        int currentSecondsCount = 0;
+        int i = 0;
+        int currentProcessIndex = 0;
+        boolean hasProcessStarted = false;
+        boolean isCurrentProcessDone = true;
+        ArrayList<Integer> doneList = new ArrayList<>(); //Finished Processes
 
-            outWait[i] = at[i-1] + bt[i-1] + outWait[i-1];
+        double totalTimeElapsed = 0;
+        double totalBurstTime = 0;
+
+        for (int value : bt) {
+            totalBurstTime += value;
+        }
+
+        boolean isProcessCalled[] = new boolean[at.length];
+        for(int j = 0; j < at.length; j++) {
+            isProcessCalled[j] = false;
+        }
+
+        boolean hasProcessArrived[] = new boolean[at.length];
+        for(int j = 0; j < at.length; j++) {
+            hasProcessArrived[j] = false;
+        }
+
+        for(int seconds = 0; !(doneList.size()==at.length); seconds++) {
+
+            for(int j = 0; j<at.length; j++) {
+                if(seconds == at[j] && seconds >= 1) {
+                    hasProcessArrived[j] = true;
+                }
+                if(hasProcessArrived[j] && !isProcessCalled[j]) {
+                    outResponse[j]++;
+                }
+            }
+
+            //If the current arrival time has not been reached
+
+            if(hasProcessStarted && i<at.length){
+
+                //CHECK IF PROCESS IS DONE
+                if(bt[i] == 0) {
+                    outputText = outputText + " " + indices[i] + " " + currentSecondsCount + "X\n";
+                    totalTimeElapsed = (double) seconds;
+
+                    currentSecondsCount = 0;
+                    isCurrentProcessDone = true;
+                    doneList.add(i); //Index of the process is added into a list of finished processes
+                    i++;
+
+                }
+
+                if(!isCurrentProcessDone) {
+                    bt[i]--;
+                    currentSecondsCount++;
+                }
+
+            }
+
+            if(isCurrentProcessDone && i<at.length) { //If the process is done, reroute the index to the next process needed to be done
+                if(seconds>=at[i]) { //Regular arrival time checking compared to seconds
+                    outputText = outputText + seconds;
+                    isProcessCalled[i] = true;
+                    hasProcessStarted = true;
+                    isCurrentProcessDone = false;
+                    seconds--;
+                }
+            }
 
         }
 
-        //Turn Around Time
+        double cpuUtilization =  (totalBurstTime / totalTimeElapsed) * 100;
+        double throughputCount = at.length / totalTimeElapsed;
 
-        for (int i = 0; i < outTurn.length; i++ ) {
+        outputText = outputText + "Total time elapsed: " + totalTimeElapsed + "ns\n";
+        outputText = outputText + "Total CPU burst time: " + totalBurstTime + "ns\n";
+        outputText = outputText + "CPU Utilization: " + cpuUtilization + "%\n";
+        outputText = outputText + "Throughput: " + throughputCount + " processes/ns\n";
 
-            outTurn[i] = bt[i] + outWait[0];
+        sortOrderByIndices();
 
+        outWait = outResponse;
+        //Out Turnaround time
+        for(int j = 0; j<dupBt.length; j++) {
+            outTurn[j] = dupBt[j] + outResponse[j];
         }
+
+        //Wait Time
+        double aveWaitTime = 0;
+        outputText = outputText + "Waiting times: \n";
+        for(int j = 1; j <= outWait.length; j++) {
+            outputText = outputText + " Process " + j + ": " + outWait[j-1] + "ns\n";
+            aveWaitTime += outWait[j-1];
+        }
+        outputText = outputText + "Average waiting time: " + (aveWaitTime/outWait.length) + " ns\n";
+
+        //Turnaround Time
+        double aveTurnTime = 0;
+        outputText = outputText + "Turnaround times: \n";
+        for(int j = 1; j <= outTurn.length; j++) {
+            outputText = outputText + " Process " + j + ": " + outTurn[j-1] + "ns\n";
+            aveTurnTime += outTurn[j-1];
+        }
+        outputText = outputText + "Average turnaround time: " + (aveTurnTime/outTurn.length) + " ns\n";
 
         //Response Time
+        double aveResTime = 0;
+        outputText = outputText + "Reponse times: \n";
+        for(int j = 1; j <= outResponse.length; j++) {
+            outputText = outputText + " Process " + j + ": " + outResponse[j-1] + "ns\n";
+            aveResTime += outResponse[j-1];
+        }
+        outputText = outputText + "Average reponse time: " + (aveResTime/outResponse.length) + " ns";
 
         return outputText;
     }
 
     public String solveAll() {
         String output = fcfsFunc();
-        return output;
-    }
-
-    //Function to print out the arrival time, burst time, and indexes in order
-    public String printOrder(int[] a, int[] b, int[] c, int[] index) {
-        String output = "";
-        for(int i = 0; i < a.length; i++) {
-            output = output + a[i] + " " + index[i] + " " + b[i] + "X";
-            output = output + "\n";
-        }
         return output;
     }
 
@@ -105,9 +205,9 @@ public class Fcfs {
                     nl[j+1] = temp;
 
                     //Sorts the indexes
-                    temp = indexes[j];
-                    indexes[j] = indexes[j+1];
-                    indexes[j+1] = temp;
+                    temp = indices[j];
+                    indices[j] = indices[j+1];
+                    indices[j+1] = temp;
 
                 }
 
@@ -116,20 +216,51 @@ public class Fcfs {
         }
     }
 
-    public int[] getAt() {
-        return at;
-    }
+    public void sortOrderByIndices() {
+        //Sort by indices time
+        for(int i = 0; i < at.length - 1; i ++) {
 
-    public int[] getBt() {
-        return bt;
-    }
+            for(int j = 0; j < at.length-i-1; j++) {
 
-    public int[] getNl() {
-        return nl;
-    }
+                if(indices[j] > indices[j+1]) {
 
-    public int[] getIndexes() {
-        return indexes;
+                    //Sorts arrival time
+                    int temp = at[j];
+                    at[j] = at[j+1];
+                    at[j+1] = temp;
+
+                    //Sorts burst time
+                    temp = bt[j];
+                    bt[j] = bt[j+1];
+                    bt[j+1] = temp;
+
+                    //Sorts nice level
+                    temp = nl[j];
+                    nl[j] = nl[j+1];
+                    nl[j+1] = temp;
+
+                    //Sorts the indexes
+                    temp = indices[j];
+                    indices[j] = indices[j+1];
+                    indices[j+1] = temp;
+
+                    temp = outWait[j];
+                    outWait[j] = outWait[j+1];
+                    outWait[j+1] = temp;
+
+                    temp = outResponse[j];
+                    outResponse[j] = outResponse[j+1];
+                    outResponse[j+1] = temp;
+
+                    temp = outTurn[j];
+                    outTurn[j] = outTurn[j+1];
+                    outTurn[j+1] = temp;
+
+                }
+
+            }
+
+        }
     }
 
 }
--- src/RoundRobin.java
+++ src/RoundRobin.java
@@ -6,9 +6,12 @@ public class RoundRobin {
     private int at[];
     private int bt[];
     private int nl[];
+    private int outWait[];
+    private int outResponse[];
+    private int outTurn[];
     private int Q;
     private int indices[];
-    private Fcfs fcfs;
+    private int[] dupBt;
 
     //a = Arrival Time , b = Burst Time , c = Nice Level , length of any of the arrays is the # of processors
     public RoundRobin(int a[], int b[], int c[], int q) {
@@ -21,7 +24,19 @@ public class RoundRobin {
         for(int i = 1; i <= at.length; i++) {
             indices[i-1] = i;
         }
-        fcfs = new Fcfs(at,bt,nl);
+        outWait = new int[at.length];
+        outTurn = new int[at.length];
+        outResponse = new int[at.length];
+
+        for(int j = 0; j < outResponse.length; j++) {
+            outResponse[j] = -1;
+        }
+
+        dupBt = new int[bt.length];
+        for (int j=0; j<dupBt.length; j++) { //initialize all wait times to 0
+            dupBt[j]=bt[j];
+        }
+
     }
 
     public String rrFunc() {
@@ -29,45 +44,63 @@ public class RoundRobin {
         //Output String
         String outputText = "";
 
-        //Output Arrangement
-        int out[] = new int[at.length];
-
-        //Output Waiting Times
-        int outWait[] = new int[at.length];
-
-        //Output Turnaround Times
-        int outTurn[] = new int[at.length];
-
-        //Output Response Times
-        int outResponse[] = new int[at.length];
-
         //Sort by arrival time
         sortOrderByArrival();
+
+        //For Round Robin
         int currentSecondsCount = 0;
         int i = 0;
         int currentProcessIndex = 0;
         boolean hasProcessStarted = false;
         boolean isCurrentProcessDone = true;
         boolean isOldProcess = false;
+        ArrayList<Integer> indexList = new ArrayList<>(); //Unfinished Processes
+        ArrayList<Integer> waitList = new ArrayList<>(); //Waiting Processes
+        ArrayList<Integer> doneList = new ArrayList<>(); //Finished Processes
 
-        ArrayList<Integer> indexList = new ArrayList<>();
-        ArrayList<Integer> doneList = new ArrayList<>();
-        HashMap<Integer, Integer> doneProcess = new HashMap<>(); //all unfinished processes that have already entered the system
+        double totalTimeElapsed = 0;
+        double totalBurstTime = 0;
+
+        for(int j = 0; j < bt.length; j++) {
+            totalBurstTime += bt[j];
+        }
+
+
+        boolean isProcessCalled[] = new boolean[at.length];
+        for(int j = 0; j < at.length; j++) {
+            isProcessCalled[j] = false;
+        }
+
+        boolean hasProcessArrived[] = new boolean[at.length];
+        for(int j = 0; j < at.length; j++) {
+            hasProcessArrived[j] = false;
+        }
+
 
         for(int seconds = 0; !(doneList.size()==bt.length); seconds++) {
 
-            //If the current arrival time has not been reached
+            for(int j = 0; j<at.length; j++) {
+                if(seconds == at[j]) {
+                    hasProcessArrived[j] = true;
+                }
+                if(hasProcessArrived[j] && !isProcessCalled[j]) {
+                    outResponse[j]++;
+                    if(!waitList.contains(j)) {
+                        waitList.add(j);
+                    }
+                }
+            }
 
+            //If the current arrival time has not been reached
             if(hasProcessStarted && currentProcessIndex<at.length){
 
                 //CHECK IF PROCESS IS DONE
                 if(bt[currentProcessIndex] == 0) {
-
                     outputText = outputText + " " + indices[currentProcessIndex] + " " + currentSecondsCount + "X\n";
-
+                    totalTimeElapsed = (double) seconds;
                     currentSecondsCount = 0;
                     isCurrentProcessDone = true;
-                    doneList.add(currentProcessIndex);
+                    doneList.add(currentProcessIndex); //Index of the process is added into a list of finished processes
                     if(!isOldProcess) {
                         i++;
                         currentProcessIndex = i;
@@ -77,9 +110,9 @@ public class RoundRobin {
 
                     outputText = outputText + " " + indices[i] + " " + currentSecondsCount + "\n";
 
-                    currentSecondsCount = 0;
-                    indexList.add(currentProcessIndex);
+                    currentSecondsCount = 0; //Reset count to 0
                     isCurrentProcessDone = true;
+                    indexList.add(currentProcessIndex); //Index of the process is added into a list of unfinished processes
                     if(!isOldProcess) {
                         i++;
                         currentProcessIndex = i;
@@ -90,33 +123,97 @@ public class RoundRobin {
                 if(!isCurrentProcessDone) {
                     bt[currentProcessIndex]--;
                     currentSecondsCount++;
+
+                    //Check for waiting
+                    if(!indexList.isEmpty()) {
+                        for(int j = 0; j <indexList.size(); j++) {
+                            outWait[indexList.get(j)]++;
+                        }
+                    }
+                    if(!waitList.isEmpty()) {
+                        for(int j = 0; j <waitList.size(); j++) {
+                            if(!isProcessCalled[i]) {
+                                outWait[waitList.get(j)]++;
+                            }
+                        }
+                    }
+                    //Check for response
                 }
 
             }
 
-            if(isCurrentProcessDone && i<at.length && currentProcessIndex<at.length) {
+            if(isCurrentProcessDone && i<at.length && currentProcessIndex<at.length) { //If the process is done, reroute the index to the next process needed to be done
 
-                outputText = outputText + seconds;
-
-                if(seconds>=at[i]) {
-
+                if(seconds>=at[i]) { //Regular arrival time checking compared to seconds
+                    outputText = outputText + seconds;
+                    for(int j = 0; j<waitList.size(); j++) {
+                        if(waitList.get(j) == i) {
+                            waitList.remove(j);
+                        }
+                    }
+                    isProcessCalled[i] = true;
                     hasProcessStarted = true;
                     currentProcessIndex = i;
                     isOldProcess = false;
+                    isCurrentProcessDone = false;
+                    seconds--;
 
-                } else if(!indexList.isEmpty()){
+                } else if(!indexList.isEmpty()){ //If no process arrival time has met the current count, check the old processes
+                    outputText = outputText + seconds;
                     currentProcessIndex = indexList.get(0);
                     indexList.remove(0);
                     hasProcessStarted = true;
                     isOldProcess = true;
+                    isCurrentProcessDone = false;
+                    seconds--;
                 }
 
-                isCurrentProcessDone = false;
-                seconds--;
             }
 
 
         }
+
+        double cpuUtilization =  (totalBurstTime / totalTimeElapsed) * 100;
+        double throughputCount = at.length / totalTimeElapsed;
+
+        outputText = outputText + "Total time elapsed: " + totalTimeElapsed + "ns\n";
+        outputText = outputText + "Total CPU burst time: " + totalBurstTime + "ns\n";
+        outputText = outputText + "CPU Utilization: " + cpuUtilization + "%\n";
+        outputText = outputText + "Throughput: " + throughputCount + " processes/ns\n";
+
+        sortOrderByIndices();
+
+        //Out Turnaround time
+        for(int j = 0; j<dupBt.length; j++) {
+            outTurn[j] = dupBt[j] + outWait[j];
+        }
+
+        //Wait Time
+        double aveWaitTime = 0;
+        outputText = outputText + "Waiting times: \n";
+        for(int j = 1; j <= outWait.length; j++) {
+            outputText = outputText + " Process " + j + ": " + outWait[j-1] + "ns\n";
+            aveWaitTime += outWait[j-1];
+        }
+        outputText = outputText + "Average waiting time: " + (aveWaitTime/outWait.length) + " ns\n";
+
+        //Turnaround Time
+        double aveTurnTime = 0;
+        outputText = outputText + "Turnaround times: \n";
+        for(int j = 1; j <= outTurn.length; j++) {
+            outputText = outputText + " Process " + j + ": " + outTurn[j-1] + "ns\n";
+            aveTurnTime += outTurn[j-1];
+        }
+        outputText = outputText + "Average turnaround time: " + (aveTurnTime/outTurn.length) + " ns\n";
+
+        //Response Time
+        double aveResTime = 0;
+        outputText = outputText + "Reponse times: \n";
+        for(int j = 1; j <= outResponse.length; j++) {
+            outputText = outputText + " Process " + j + ": " + outResponse[j-1] + "ns\n";
+            aveResTime += outResponse[j-1];
+        }
+        outputText = outputText + "Average reponse time: " + (aveResTime/outResponse.length) + " ns";
 
         return outputText;
 
@@ -154,6 +251,53 @@ public class RoundRobin {
                     temp = indices[j];
                     indices[j] = indices[j+1];
                     indices[j+1] = temp;
+
+                }
+
+            }
+
+        }
+    }
+
+    public void sortOrderByIndices() {
+        //Sort by indices time
+        for(int i = 0; i < at.length - 1; i ++) {
+
+            for(int j = 0; j < at.length-i-1; j++) {
+
+                if(indices[j] > indices[j+1]) {
+
+                    //Sorts arrival time
+                    int temp = at[j];
+                    at[j] = at[j+1];
+                    at[j+1] = temp;
+
+                    //Sorts burst time
+                    temp = bt[j];
+                    bt[j] = bt[j+1];
+                    bt[j+1] = temp;
+
+                    //Sorts nice level
+                    temp = nl[j];
+                    nl[j] = nl[j+1];
+                    nl[j+1] = temp;
+
+                    //Sorts the indexes
+                    temp = indices[j];
+                    indices[j] = indices[j+1];
+                    indices[j+1] = temp;
+
+                    temp = outWait[j];
+                    outWait[j] = outWait[j+1];
+                    outWait[j+1] = temp;
+
+                    temp = outResponse[j];
+                    outResponse[j] = outResponse[j+1];
+                    outResponse[j+1] = temp;
+
+                    temp = outTurn[j];
+                    outTurn[j] = outTurn[j+1];
+                    outTurn[j+1] = temp;
 
                 }
 
